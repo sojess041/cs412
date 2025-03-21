59972ab1b0a29fa7013444d381a2034a48fa9f37
Jess So <jyuloso@bu.edu>
2025-03-21 14:16:52 -0400

    Updating files for A8 Task 3

--- restaurant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_page, name='home'), 
-    path('order/', views.order_form, name='order'), 
-    path('main/', views.main_page, name='main'), 
-    path('submit/', views.submit_order, name='submit'),
-]
